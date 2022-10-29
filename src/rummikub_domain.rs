e2fd96f577f7fb98bb865e099658bedcd2978ad9
Jaco_WSL2_Omen <jaredcombs5@live.com>
2022-10-28 23:46:13 -0700

    Domain Modeling via types continues to progress, tests pass

--- src/rummikub_domain.rs
+++ src/rummikub_domain.rs
@@ -1,13 +1,27 @@
+
+#[allow(dead_code)]
 pub mod types {
+
+    //106 tiles in the game, including 104 numbered tiles (valued 1 to 13 in four different colors, two copies of each) and two jokers
+
+    use std::collections::HashSet;
+    // TODO , I don't actually know what to do with these but good example for crates
+    use strum_macros::EnumString;
+    use strum::IntoEnumIterator;
+    use strum_macros::EnumIter;
+    use crate::rummikub_domain::types::Tile::{RegularTile, JokersWild};
+
+    #[derive(EnumIter, Debug, PartialEq, Eq, Hash, Copy, Clone)]
     enum Color {
         Red,
         Blue,
         Orange,
         Black,
-        Colorless, // TODO Should this exist? Thinking about monoids
+        // AnyColor, // TODO Should this exist? Thinking about monoids and or Wilds
     }
 
-    enum Cardinality {
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, EnumString, EnumIter, Hash, Copy, Clone)]
+    enum Number {
         One,
         Two,
         Three,
@@ -23,6 +37,110 @@ pub mod types {
         Thirteen,
         //TODO, it's tempting to put u8's here, but for now, I'm not going to because
         // I want to make illegal states unrepresentable.
-        // Maybe later I can add some traits or ordering or something
+    }
+
+    #[derive(Debug, Hash, Clone)]
+    enum Tile {
+        RegularTile(Color, Number),
+        JokersWild  // Todo, do they have a color?
+    }
+
+    enum Set {
+        // There are two kinds of sets, either a group or a run
+        Group,
+        Run
+    }
+
+    struct Group {
+        //A set of either three or four tiles of the same number in different colors.
+        members: HashSet<Tile>
+    }
+    impl Group {
+        fn new(candidates: Vec<Tile>) -> Option<Group> {
+            // TODO something like parsing with the new operator here to enforce characteristics
+            let mut foo = HashSet::new();
+            Option::Some(Group { members: foo })
+        }
+    }
+
+    struct Run {
+        // a set of three or more consecutive numbers all in the same color.
+        // The number 1 is always played as the lowest number, it cannot follow the number 13.
+        members: Vec<Tile> // TODO, Consider a LinkedList
+    }
+
+
+    // #[derive(PartialEq)] // TODO interesting behavior of Vectors
+    struct Boneyard {
+        // TODO, new constructor here is important. Perhaps a map? instead?
+        bones: Vec<Tile>
+    }
+
+    impl Boneyard {
+        fn new() -> Option<Boneyard> {
+            let mut jokers: Vec<Tile> = Vec::new();
+            jokers.push(JokersWild);
+            jokers.push(JokersWild);
+
+            for color in Color::iter() {
+                for num in Number::iter() {
+                    println!("Look a Tile!: {:?}", RegularTile(color, num))
+                }
+
+            }
+            return Option::None
+        }
+
+        fn draw_one() -> (Tile, Boneyard) {
+            // TODO here, but with the idea of immutablity, when drawing, we get a whole new boneyard
+            (RegularTile(Color::Red, Number::Twelve), Boneyard { bones: Vec::new()})
+        }
+    }
+
+    struct PlayerRack {
+        tiles: Vec<Tile>
+    }
+
+    struct Table {
+        // Publicly viewable and mutable by all players, has all the sets that have been placed
+        sets: Vec<Set>
+    }
+
+    //Whoa! Unit tests go IN THE SAME FILE
+    #[cfg(test)]
+    mod tests {
+        use crate::rummikub_domain::types::{Color, Number, Tile};
+        use crate::rummikub_domain::types::Color::Black;
+
+        #[test]
+        fn it_works() {
+            let result = 2 + 2;
+            assert_eq!(result, 4);
+        }
+
+
+        #[test]
+        fn print_some_types() {
+            let color: Color = Black;
+            assert_eq!(color, Color::Black);
+            println!("Dingus {:?}", color);
+            let tile: Tile = Tile::RegularTile(Color::Black, Number::One);
+            // TOdo iterate throught a list of enums
+            println!("How bout a tile: {:?}", tile)
+
+        }
+    }
+
+
+    #[cfg(test)]
+    mod test_boneyard {
+        use crate::rummikub_domain::types::Boneyard;
+
+        #[test]
+        fn build_boneyard() {
+            let mut bones = Boneyard::new();
+
+        }
+
     }
 }
